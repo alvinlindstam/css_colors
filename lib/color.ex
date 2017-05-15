f7b71776543e01a1d7566eb41ff8e1ae8d717d58
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-05-15 20:09:27 +0200

    Add functions to get named attributes for colors

--- lib/color.ex
+++ lib/color.ex
@@ -3,6 +3,9 @@ defmodule CssColors.Color do
 
   alias CssColors.{RGB, HSL}
 
+  @rgb_fields [:red, :green, :blue, :alpha]
+  @hsl_fields [:hue, :saturation, :lightness, :alpha]
+
   def rgb(color=%RGB{}) do
     color
   end
@@ -18,6 +21,25 @@ defmodule CssColors.Color do
     {h, s, l} = RGB.to_hsl(rgb)
     hsl(h, s, l, alpha)
   end
+
+  # Getter functions
+  def red(color), do: get_attribute(color, :red)
+  def green(color), do: get_attribute(color, :green)
+  def blue(color), do: get_attribute(color, :blue)
+  def hue(color), do: get_attribute(color, :hue)
+  def saturation(color), do: get_attribute(color, :saturation)
+  def lightness(color), do: get_attribute(color, :lightness)
+  def alpha(color), do: get_attribute(color, :alpha)
+
+  def get_attribute(color, key) do
+    color
+    |> cast_color_by_attribute(key)
+    |> Map.fetch!(key)
+  end
+
+  defp cast_color_by_attribute(color, :alpha), do: color
+  defp cast_color_by_attribute(color, attribute) when attribute in @rgb_fields, do: rgb(color)
+  defp cast_color_by_attribute(color, attribute) when attribute in @hsl_fields, do: hsl(color)
 
   defdelegate rgb(red, green, blue), to: RGB
   defdelegate rgb(red, green, blue, alpha), to: RGB
