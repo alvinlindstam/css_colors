ccba3c2f88018c83aac0eb04cd862cc77ce8da67
Alvin Lindstam <alvin.lindstam@gmail.com>
2018-03-05 23:10:47 +0100

    Remove duplicates from color literal map

--- lib/CssColors/parser.ex
+++ lib/CssColors/parser.ex
@@ -6,22 +6,6 @@ defmodule CssColors.Parser do
   @alpha_regex Regex.compile!("^#{pattern},#{pattern},#{pattern},\\s*(\\d+\\.?\\d*)\\s*\\)$")
 
   @named_colors %{
-    "black"	=> "#000000",	# 0,0,0
- 	 	"silver"	=> "#C0C0C0",	# 192,192,192
- 	 	"gray"	=> "#808080",	# 128,128,128
- 	 	"white"	=> "#FFFFFF",	# 255,255,255
- 	 	"maroon"	=> "#800000",	# 128,0,0
- 	 	"red"	=> "#FF0000",	# 255,0,0
- 	 	"purple"	=> "#800080",	# 128,0,128
- 	 	"fuchsia"	=> "#FF00FF",	# 255,0,255
- 	 	"green"	=> "#008000",	# 0,128,0
- 	 	"lime"	=> "#00FF00",	# 0,255,0
- 	 	"olive"	=> "#808000",	# 128,128,0
- 	 	"yellow"	=> "#FFFF00",	# 255,255,0
- 	 	"navy"	=> "#000080",	# 0,0,128
- 	 	"blue"	=> "#0000FF",	# 0,0,255
- 	 	"teal"	=> "#008080",	# 0,128,128
- 	 	"aqua"	=> "#00FFFF",	# 0,255,255
     "aliceblue"	=> "#F0F8FF",	# 240,248,255
  	 	"antiquewhite"	=> "#FAEBD7",	# 250,235,215
  	 	"aqua"	=> "#00FFFF",	# 0,255,255
